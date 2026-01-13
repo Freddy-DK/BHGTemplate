1b554dec5944ab21efda31717384dd0824387a4c
Freddy Kristiansen <fkr@freddy.dk>
2026-01-13 14:12:21 +0100

    run only on Windows and set artifact cache folder

--- .AL-Go/pipelineinitialize.ps1
+++ .AL-Go/pipelineinitialize.ps1
@@ -1,3 +1,11 @@
+# Initialize BC Service and set parent variables
+# Only if running on GitHub Hosted Windows Runners
+
+if ($env:RUNNER_OS -ne "Windows") {
+    Write-Host "Not running on Windows GitHub Hosted Runner. Skipping pipelineinitialize.ps1"
+    return
+}
+
 Start-Job -ScriptBlock {
     # Start BC services in the job to avoid waiting the main job
     . (Join-Path $ENV:GITHUB_WORKSPACE 'Scripts/MyFunctions.ps1')
@@ -13,3 +21,6 @@ Set-Variable -Name 'bcAuthContext' -value $bcAuthContext -scope 1
 
 Write-Host "Set parent variable 'environment'"
 Set-Variable -Name 'environment' -value "http://localhost/BC" -scope 1
+
+Write-Host "Set parent variable 'artifactCachePath'"
+Set-Variable -Name 'artifactCachePath' -value "C:\compilerfolder.cache" -scope 1
